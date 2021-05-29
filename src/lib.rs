89a82f2b4fdff9d7928b3c14dd389157babd2094
Hocine <hocine.hammouche@hotmail.com>
2021-05-29 14:09:57 +0200

    Add a new module for random
    The functions that launch games and rounds randomly have been moved to a module

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 pub mod board{
-	use rand::{seq::SliceRandom, thread_rng, Rng};
-
 	#[derive(Clone, Copy, PartialEq)]
 	pub enum Token { Red, Yellow, Empty }
 
-	struct Board { body: [[Token; 7]; 6] }
+	pub struct Board { body: [[Token; 7]; 6] }
 
 	impl Board {
-		fn new() -> Board {
+		pub fn new() -> Board {
 			let f = Token::Empty;
 			Board {
 				body: [
@@ -20,7 +18,7 @@ pub mod board{
 				],
 			}
 		}
-		fn is_full(&self) -> bool {
+		pub fn is_full(&self) -> bool {
 			for row in self.body.iter() {
 				for cell in row {
 					if cell == &Token::Empty { return false; }
@@ -28,7 +26,7 @@ pub mod board{
 			}
 			true
 		}
-		fn check_winner(&self) -> Token {
+		pub fn check_winner(&self) -> Token {
 			// Verification for each player
 			for player_token in [Token::Red, Token::Yellow].iter() {
 				// Check horizontally
@@ -91,7 +89,7 @@ pub mod board{
 			}
 			Token::Empty
 		}
-		fn player_stroke(&mut self, token: Token, col: usize) -> Option<bool> {
+		pub fn player_stroke(&mut self, token: Token, col: usize) -> Option<bool> {
 			// check if the column number is valid
 			if !(0..7).contains(&col) { return None; }
 			// check if the column is not full
@@ -106,9 +104,16 @@ pub mod board{
 			None
 		}
 	}
+}
 
-	// start a random game
-	pub fn rand_game(rand_first_player: bool) -> Token {
+pub mod random{
+	use super::board::{Token, Board};
+	use rand::{seq::SliceRandom, thread_rng, Rng};
+	use std::sync::mpsc::Sender;
+	use std::time::Instant;
+
+	// Start a round between two fictitious players who play random moves
+	pub fn round(rand_first_player: bool) -> Token {
 		let mut board = Board::new();
 		// the pos_list vector contains the column numbers which are each present the number of times we can enter a token in said column
 		let mut pos_list: Vec<usize> = Vec::from([0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5]);
@@ -122,5 +127,46 @@ pub mod board{
 		}
 		// returns the winner of the game
 		board.check_winner()
+	}
+
+	// Launches a number of rounds between two fictitious players who play random moves
+	fn rounds(number_rounds: u64, tx: Sender<[(Token, u64); 3]>, rand_first_player: bool) {
+		let mut res: [(Token, u64); 3] = [(Token::Red, 0), (Token::Yellow, 0), (Token::Empty, 0)];
+		for _i in 0..number_rounds {
+			match round(rand_first_player) {
+				Token::Red => res[0].1 += 1,
+				Token::Yellow => res[1].1 += 1,
+				Token::Empty => res[2].1 += 1,
+			};
+		}
+		tx.send(res).unwrap();
+	}
+
+	// Throws threads that will throw a certain number of rounds
+	pub fn game_session(number_thread: u32, number_rounds: u64, rand_first_player: bool)  -> [(Token, u64); 3] {
+		let total_rounds = number_rounds * number_thread as u64;
+		let mut res: [(Token, u64); 3] = [(Token::Red, 0), (Token::Yellow, 0), (Token::Empty, 0)];
+
+		let now = Instant::now();
+
+		let mut children = vec![];
+		let (tx, rx) = std::sync::mpsc::channel();
+		for _i in 0..number_thread {
+			let tx_copy = std::sync::mpsc::Sender::clone(&tx);
+			children.push(std::thread::spawn(move || rounds(number_rounds, tx_copy, rand_first_player)));
+		}
+		std::mem::drop(tx);
+		for tmp in rx{
+			res[0].1 += tmp[0].1;
+			res[1].1 += tmp[1].1;
+			res[2].1 += tmp[2].1;
+		}
+		let exec_time = now.elapsed().as_millis();
+		println!("finished after {} milliseconds or {:.2} seconds or {:.2} minutes.", exec_time, exec_time as f64/1000.0, exec_time as f64/1000.0/60.0);
+		println!("\nresult of {} game : ", total_rounds);
+		println!("\t{:.3}% victory for the red token.", (res[0].1 as f64)*100.0/(total_rounds as f64));
+		println!("\t{:.3}% victory for the yellow token.", (res[1].1 as f64)*100.0/(total_rounds as f64));
+		println!("\t{:.3}% draw.", (res[2].1 as f64)*100.0/(total_rounds as f64));
+		res
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,7 @@
 mod lib;
 
-use std::{thread, time::Instant};
-use std::sync::mpsc::Sender;
-use lib::board::{Token, rand_game};
+use lib::random;
 
 fn main() {
-    launch_games_thread(10, 100_000, true);
-}
-
-// Start a number of random games and record the number of wins for each in a table
-fn launch_rounds(number_rounds: u64, tx: Sender<[(Token, u64); 3]>, rand_first_player: bool) {
-    let mut res: [(Token, u64); 3] = [(Token::Red, 0), (Token::Yellow, 0), (Token::Empty, 0)];
-    for _i in 0..number_rounds {
-        match rand_game(rand_first_player) {
-            Token::Red => res[0].1 += 1,
-            Token::Yellow => res[1].1 += 1,
-            Token::Empty => res[2].1 += 1,
-        };
-    }
-    tx.send(res);
-}
-// launch threads of the launch_rounds function then get their result and put them in a single array
-fn launch_games_thread(number_thread: u32, number_rounds: u64, rand_first_player: bool) -> [(Token, u64); 3] {
-    let total_rounds = number_rounds * number_thread as u64;
-    let mut res: [(Token, u64); 3] = [(Token::Red, 0), (Token::Yellow, 0), (Token::Empty, 0)];
-
-    let now = Instant::now();
-
-    let mut children = vec![];
-    let (tx, rx) = std::sync::mpsc::channel();
-    for _i in 0..number_thread {
-        let tx_copy = std::sync::mpsc::Sender::clone(&tx);
-        children.push(thread::spawn(move || launch_rounds(number_rounds, tx_copy, rand_first_player)));
-    }
-    std::mem::drop(tx);
-    for tmp in rx{
-        res[0].1 += tmp[0].1;
-        res[1].1 += tmp[1].1;
-        res[2].1 += tmp[2].1;
-    }
-    let exec_time = now.elapsed().as_millis();
-    println!("finished after {} milliseconds or {:.2} seconds or {:.2} minutes.", exec_time, exec_time as f64/1000.0, exec_time as f64/1000.0/60.0);
-    println!("\nresult of {} game : ", total_rounds);
-    println!("\t{:.3}% victory for the red token.", (res[0].1 as f64)*100.0/(total_rounds as f64));
-    println!("\t{:.3}% victory for the yellow token.", (res[1].1 as f64)*100.0/(total_rounds as f64));
-    println!("\t{:.3}% draw.", (res[2].1 as f64)*100.0/(total_rounds as f64));
-    res
+    random::game_session(15, 100_000_000, false);
 }
