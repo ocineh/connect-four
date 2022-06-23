d451b217bc13360687ee1ed50b61ad3298faec14
Hocine <ocineh@hotmail.com>
2022-06-23 22:52:21 +0200

    Add an interface in command line

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,46 @@
+use structopt::StructOpt;
+
+use lib::{game::*, random::*};
+
 mod lib;
-use lib::game;
+
+#[derive(StructOpt, Debug)]
+enum Shell {
+	Bash,
+	Fish,
+	PowerShell,
+	Zsh,
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "Connect four")]
+/// A game where you need to connect four tokens to win, but you can only place tokens in a column.
+///
+/// When you place a token it will fall down to the lowest available space.
+enum Cmd {
+	/// Play a game against a fellow human
+	Human,
+	/// Play a game against the computer (random stroke)
+	Computer,
+	/// Play games with the computer against himself (random stroke)
+	Random {
+		#[structopt(short, long, default_value = "1")]
+		/// The number of threads to use
+		threads: u32,
+
+		#[structopt(short, long, default_value = "100")]
+		/// The number of rounds to play per thread
+		rounds: u64,
+	},
+}
 
 fn main() {
-    game::against_computer();
+	let cmd: Cmd = Cmd::from_args();
+	match cmd {
+		Cmd::Human => against_another_player(),
+		Cmd::Computer => against_computer(),
+		Cmd::Random { threads, rounds } => {
+			game_session(threads, rounds, true);
+		}
+	}
 }
